6ab105881b220c21e1c4ca28cb2a80c5cea31192
ellacastafiore <ellacastafiore@gmail.com>
2017-02-13 21:34:39 -0500

    Version 1 du script demandé
    Avec seulement des listes

---
+++ devoir1v1.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+#tous les médecins avant l'an 2000 feront partie de la liste 1
+l1 = list(range(30000,100000,1))
+#tous les médecins de 2000 à ajd font partie de la liste 2
+l2 = list(range(00000,18000,1))
+
+l3 = l1+l2 
+print(l3)
